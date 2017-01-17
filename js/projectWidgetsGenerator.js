427c0b1b500824b05b5124ba1ae8d4039aff48dd
swapchan1 <swapchan@umail.iu.edu>
2017-01-16 21:19:42 -0500

    new
    green backgrnd

--- js/projectWidgetsGenerator.js
+++ js/projectWidgetsGenerator.js
@@ -32,7 +32,7 @@
                 categoryExp: "widgetCntrl.isView(3) || widgetCntrl.isView(1)",
                 widgetTitle: "DinnerCall",
                 widgetDescription: 'UX Design & Evaluation',
-                pageLink: "dinnercall.html",
+                pageLink: "dinrcall.html",
                 widgetImage:"assets/images/portfolio/campusnav/logo.png"
             },
             
