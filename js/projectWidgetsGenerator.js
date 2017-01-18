e091f254227021b547f58e0f87729ee20ce55558
swapchan1 <swapchan@umail.iu.edu>
2017-01-18 09:13:27 +0530

    removed in campus locator

--- js/projectWidgetsGenerator.js
+++ js/projectWidgetsGenerator.js
@@ -21,7 +21,7 @@
     var projectWidgets = 
         {
            {
-                categoryExp: "widgetCntrl.isView(3) || widgetCntrl.isView(1)",
+                categoryExp: "widgetCntrl.isView(2) || widgetCntrl.isView(1)",
                 widgetTitle: "DinnerCall",
                 widgetDescription: 'UX Design & Evaluation',
                 pageLink: "dinrcall.html",
@@ -29,7 +29,7 @@
             },
             
             {
-                categoryExp: "widgetCntrl.isView(4) || widgetCntrl.isView(1)",
+                categoryExp: "widgetCntrl.isView(3) || widgetCntrl.isView(1)",
                 widgetTitle: "Musical Moods - Visual Analytics",
                 widgetDescription: 'Arduino Physical Prototype',
                 pageLink: "musicalmoods.html",
@@ -37,7 +37,7 @@
             },
             
             {
-                categoryExp: "widgetCntrl.isView(5) || widgetCntrl.isView(1)",
+                categoryExp: "widgetCntrl.isView(4) || widgetCntrl.isView(1)",
                 widgetTitle: "EduAssist",
                 widgetDescription: 'HCI Research Study',
                 pageLink: "EduAssist.html",
