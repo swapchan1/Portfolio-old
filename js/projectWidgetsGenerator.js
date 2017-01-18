a3ad4c73e99b7b107ad9cf7de8408dcc4a6d40a9
swapchan1 <swapchan@umail.iu.edu>
2017-01-18 09:47:59 +0530

    put in campus locator

--- js/projectWidgetsGenerator.js
+++ js/projectWidgetsGenerator.js
@@ -1,4 +1,4 @@
-function(){
+(function(){
     
     var widgetGeneratorApp = angular.module('ProjectWidgetsGenerator', []);
     
@@ -20,28 +20,36 @@ function(){
     
     var projectWidgets = 
         [
-           {
+            {
                 categoryExp: "widgetCntrl.isView(2) || widgetCntrl.isView(1)",
-                widgetTitle: "DinnerCall",
-                widgetDescription: 'UX Design & Evaluation',
-                pageLink: "dinrcall.html",
-                widgetImage:"Projects/Dinnercall/dp.jpeg"
+                widgetTitle: "In-Campus Locator",
+                widgetDescription: 'A Smart Kitchen Experience',
+                pageLink:"campusnav.html" ,
+                widgetImage:"assets/images/portfolio/campusnav/logo.png"
             },
             
             {
                 categoryExp: "widgetCntrl.isView(3) || widgetCntrl.isView(1)",
-                widgetTitle: "Musical Moods - Visual Analytics",
-                widgetDescription: 'Arduino Physical Prototype',
-                pageLink: "musicalmoods.html",
-                widgetImage:"Projects/MusicalMoods/dp.jpg"
+                widgetTitle: "DinnerCall",
+                widgetDescription: 'UX Design & Evaluation',
+                pageLink: "dinnercall.html",
+                widgetImage:"assets/images/portfolio/campusnav/logo.png"
             },
             
             {
                 categoryExp: "widgetCntrl.isView(4) || widgetCntrl.isView(1)",
+                widgetTitle: "Musical Moods - Visual Analytics",
+                widgetDescription: 'Arduino Physical Prototype',
+                pageLink: "musicalmoods.html",
+                widgetImage:"assets/images/portfolio/campusnav/logo.png"
+            },
+            
+            {
+                categoryExp: "widgetCntrl.isView(5) || widgetCntrl.isView(1)",
                 widgetTitle: "EduAssist",
                 widgetDescription: 'HCI Research Study',
                 pageLink: "EduAssist.html",
-                widgetImage:"Projects/Edu-Assist/dp.jpg"
+                widgetImage:"assets/images/portfolio/campusnav/logo.png"
             }
         ];
 })();
